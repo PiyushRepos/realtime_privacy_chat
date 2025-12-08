2e796a1df43ee7f9aa5afb8bba84793f8f51a67b
Piyush Kumar <piyush2004.dev@gmail.com>
2025-12-08 21:49:27 +0530

    add base url in env

--- src/app/lib/client.ts
+++ src/app/lib/client.ts
@@ -2,4 +2,6 @@ import { treaty } from "@elysiajs/eden";
 import type { App } from "@/app/api/[[...slug]]/route";
 
 // this require .api to enter /api prefix
-export const client = treaty<App>("localhost:3000").api;
+export const client = treaty<App>(
+  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
+).api;
